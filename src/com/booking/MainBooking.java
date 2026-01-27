be0af4667e2d1f2e66dc86a5e9b0ac125554a545
Juliensz-alt <sanchezjulien05@gmail.com>
2026-01-27 14:20:49 +0100

    md

--- src/com/booking/MainBooking.java
+++ src/com/booking/MainBooking.java
@@ -449,7 +449,7 @@ public class MainBooking {
         System.out.println("🔍 Hébergements ≤ 200€ : " + 
             collection.rechercherParPrixMax(200.0).size());
         System.out.println("🔍 Hébergements capacité ≥ 4 : " + 
-            ollection.rechercherParCapacite(4).size());
+            Collection.rechercherParCapacite(4).size());
 
         System.out.println("\n✅ Démonstration des collections terminée\n");
     }
